b08e21620ec7ed1feaf85647907bc5826f3bc918
2khs21 <2khs21@naver.com>
2025-08-11 15:25:01 +0900

    SET UP : initializr spring boot

---
+++ src/main/kotlin/com/example/language_pet/LanguagePetApplication.kt
@@ -0,0 +1,11 @@
+package com.example.language_pet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LanguagePetApplication
+
+fun main(args: Array<String>) {
+	runApplication<LanguagePetApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/language_pet/LanguagePetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.language_pet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LanguagePetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
